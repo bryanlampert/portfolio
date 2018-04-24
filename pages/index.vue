272f4804d1243f6ceb8a90b38d4f0a639d33f2c8
Bryan <bryanlampert@gmail.com>
2018-04-24 14:27:51 -0400

    fix ssr error

--- pages/index.vue
+++ pages/index.vue
@@ -1,30 +1,32 @@
 <template>
-  <div class="wrapper">
-    <vue-particles
-      color="#42b983"
-      :particleOpacity="0.7"
-      :particlesNumber="120"
-      shapeType="circle"
-      :particleSize="4"
-      linesColor="#6345ed"
-      :linesWidth="1"
-      :lineLinked="true"
-      :lineOpacity="0.4"
-      :linesDistance="150"
-      :moveSpeed="3"
-      hoverMode="grab"
-      :hoverEffect="true"
-      :clickEffect="true"
-      clickMode="repulse"
-    >
-    </vue-particles>
-    <section class="section is-large">
-      <div class="container has-text-centered">
-        <h1 class="title">Hi, I'm Bryan Lampert</h1>
-        <h2 class="subtitle">a Web Developer based in Toronto, Canada.</h2>
-      </div>
-    </section>
-  </div>
+  <no-ssr>
+    <div class="wrapper">
+      <vue-particles
+        color="#42b983"
+        :particleOpacity="0.7"
+        :particlesNumber="120"
+        shapeType="circle"
+        :particleSize="4"
+        linesColor="#6345ed"
+        :linesWidth="1"
+        :lineLinked="true"
+        :lineOpacity="0.4"
+        :linesDistance="150"
+        :moveSpeed="3"
+        hoverMode="grab"
+        :hoverEffect="true"
+        :clickEffect="true"
+        clickMode="repulse"
+      >
+      </vue-particles>
+      <section class="section is-large">
+        <div class="container has-text-centered">
+          <h1 class="title">Hi, I'm Bryan Lampert</h1>
+          <h2 class="subtitle">a Web Developer based in Toronto, Canada.</h2>
+        </div>
+      </section>
+    </div>
+  </no-ssr>
 </template>
 
 <style scoped>
